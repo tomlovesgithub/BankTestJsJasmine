c62f38b7533be07c59372bfec4b4f95e3a42c4cf
tomlovesgithub <44227249+tomlovesgithub@users.noreply.github.com>
2019-04-12 17:01:50 +0100

    Update BankSpec.js

--- spec/BankSpec.js
+++ spec/BankSpec.js
@@ -5,16 +5,20 @@ describe('Bank', function () {
     bank = new Bank()
   })
 
-  it('should have a balance', function () {
+//   i want my bank account to hold a balance,
+//   defaulting at 0
+  
+  it('should have a balance, defaulting at 0', function () {
     expect(bank.balance).toEqual(0)
   })
-
+  
+//   deposit function raises balance
   it('deposit should raise balance', function () {
     expect(bank.balance).toEqual(0)
     bank.deposit(50)
     expect(bank.balance).toEqual(50)
   })
-
+// withdraw lowers balance
   it('withdraw should decrease balance', function () {
     expect(bank.balance).toEqual(0)
     bank.deposit(50)
